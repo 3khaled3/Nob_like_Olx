49144618892168ed185545d9c93c48eda047756f
3khaled3 <ff.tark123@gmail.com>
2023-08-02 08:32:53 +0300

    createSaleItem

--- lib/core/ProdactWidget/saleItem.dart
+++ lib/core/ProdactWidget/saleItem.dart
@@ -1,124 +1,137 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-
 import 'package:cached_network_image/cached_network_image.dart';
-
 import '../../features/home/data/product.dart';
 import '../widget/CustomcircleIconButtom.dart';
 
 class SaleItem extends StatelessWidget {
-  final Product product;
+  // final Product product;
 
   const SaleItem({
     super.key,
-    required this.product,
+    // required this.product,
   });
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-          padding: const EdgeInsets.only(top: 8, bottom: 8, right: 4, left: 4),
-          child: GestureDetector(
-            onTap: () {
-            
-            },
-            child: Stack(
-              children: [
-                Container(
-                  width: 170,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.white),
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
-                            child: CachedNetworkImage(
-                          fit: BoxFit.cover,
-                          width: 162,
-                          height: 184,
-                          imageUrl: product.thumbnail,
-                          progressIndicatorBuilder:
-                              (context, url, downloadProgress) =>
-                                  Text("wait"),
-                          errorWidget: (context, url, error) =>
-                              const Icon(Icons.error),
-                        )),
-               
-                        Text(
-                          product.brand,
-                          style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 11,
-                              fontWeight: FontWeight.normal),
-                        ),
-                        Text(
-                          product.title,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600),
-                        ),
-                        Row(
-                          children: [
-                            SizedBox(
-                              height: 16,
-                              child: Text(
-                                "${product.price}\$",
-                                style: const TextStyle(
-                                    color: Colors.red,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.normal),
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                Positioned(
-                    left: 131,
-                    height: 370,
-                    child: 
-                        circleIconButtom(
-                            iconColor:  Colors.grey,
-                            icon: 
-                                 Icons.favorite_border_rounded,
-                            onTap: () async {
-                              // await favCubit.addTofav(product.id, context);
-                              // ignore: use_build_context_synchronously
-                            })),
-                Positioned(
-                  left: 13,
-                  top: 13,
-                  child: Container(
-                    width: 40,
-                    height: 24,
-                    decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Center(
-                      child: Text(
-                        "-${product.discountPercentage.toInt()}%",
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 11,
-                            fontWeight: FontWeight.w600),
+    return SizedBox(
+      child: Container(
+        color: Colors.white,
+        child: GestureDetector(
+          onTap: () {},
+          child: Stack(
+            children: [
+              Container(
+                height: MediaQuery.sizeOf(context).height * .3,
+                width: MediaQuery.sizeOf(context).width * .43,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.white),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                        child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset(
+                        "assets/te.jpg",
+                        fit: BoxFit.cover,
+                        width: MediaQuery.sizeOf(context).width * .43,
+                        height: MediaQuery.sizeOf(context).height * .19,
                       ),
+                    )
+                        //      CachedNetworkImage(
+                        //   fit: BoxFit.cover,
+                        //   width: 162,
+                        //   height: 184,
+                        //   imageUrl: product.thumbnail,
+                        //   progressIndicatorBuilder:
+                        //       (context, url, downloadProgress) =>
+                        //           Text("wait"),
+                        //   errorWidget: (context, url, error) =>
+                        //       const Icon(Icons.error),
+                        // )
+                        ),
+                    Text(
+                      "Iphone 14 Pro Max",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400),
                     ),
+                    Text(
+                      "215 \$",
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: 40,
+                          height: 24,
+                          decoration: BoxDecoration(
+                              color: Color(0xffEFEFEF),
+                              borderRadius: BorderRadius.circular(16)),
+                          child: Center(
+                            child: Text(
+                              "Used",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          width: 40,
+                          height: 24,
+                          decoration: BoxDecoration(
+                              color: Color(0xffEFEFEF),
+                              borderRadius: BorderRadius.circular(16)),
+                          child: Center(
+                            child: Text(
+                              "08/10",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                right: 16,
+                top: 10,
+                child: SizedBox(
+                  height: 32,
+                  width: 32,
+                  child: circleIconButtom(
+                    iconColor: Colors.white,
+                    icon: Icons.favorite_border_rounded,
+                    onTap: () async {
+                      // await favCubit.addTofav(product.id, context);
+                      // ignore: use_build_context_synchronously
+                    },
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        );
-      }
+        ),
+      ),
+    );
   }
+}
