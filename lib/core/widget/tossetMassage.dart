30fee4cefcc96423b8e12075a2676a4c24b366d9
3khaled3 <ff.tark123@gmail.com>
2023-08-05 10:53:38 +0300

    editshowToastMessageDuration

--- lib/core/widget/tossetMassage.dart
+++ lib/core/widget/tossetMassage.dart
@@ -4,13 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 void showToastMessage(String message, Color backgroundColor) {
-  print("aaaaaaaaaaaaaaaaaaaaaaaaa${message.length*.4.round()}");
+  int messageLength = message.length;
+  int duration = messageLength <= 10 ? 3 : (messageLength ~/ 10) + 3;
+
   Fluttertoast.showToast(
     msg: message,
     toastLength: Toast.LENGTH_SHORT,
     gravity: ToastGravity.BOTTOM,
-    timeInSecForIosWeb: 
-    5 ,
+    timeInSecForIosWeb: duration,
     backgroundColor: backgroundColor,
     textColor: Colors.white,
     fontSize: 16.0,
