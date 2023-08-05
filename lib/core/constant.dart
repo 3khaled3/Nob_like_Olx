0d6fa561594f73f593cfdbd2fcccb38b5532cbea
3khaled3 <ff.tark123@gmail.com>
2023-08-05 11:36:17 +0300

    AddcategoriesList

---
+++ lib/core/constant.dart
@@ -0,0 +1,99 @@
+Map<String, dynamic> categories = {
+  'Electronics and Gadgets': [
+    'Smartphones',
+    'Laptops and Computers',
+    'Cameras and Photography equipment',
+    'Audio and Headphones',
+    'Wearable Devices',
+  ],
+  'Fashion and Apparel': [
+    'Clothing (Men\'s, Women\'s, Kids)',
+    'Footwear',
+  ],
+  'Home and Furniture': [
+    'Furniture',
+    'Home Decor',
+    'Kitchen Appliances',
+    'Bedding and Linens',
+  ],
+  'Beauty and Personal Care': [
+    'Skincare',
+    'Makeup',
+    'Haircare',
+    'Perfumes and Fragrances',
+    'Personal Care Appliances',
+  ],
+  'Health and Wellness': [
+    'Vitamins and Supplements',
+    'Fitness Equipment',
+    'Health Monitors',
+    'Sports Gear',
+  ],
+  'Books and Media': [
+    'Books',
+    'Music',
+    'Movies',
+  ],
+  'Toys and Games': [
+    'Toys for Kids',
+    'Board Games',
+    'Video Games ',
+  ],
+  'Automotive': [
+    'Car Parts and Accessories',
+    'Car Care Products',
+    'Motorcycle Gear',
+  ],
+  'Sports and Outdoors': [
+    'Outdoor Equipment ',
+    'Athletic Clothing',
+    'Exercise Equipment',
+  ],
+  'Groceries and Food': [
+    'Fresh Produce',
+    'Packaged Foods',
+    'Snacks and Beverages',
+  ],
+  'Jewelry and Watches': [
+    'Rings',
+    " Necklaces",
+    " Bracelets",
+    'Luxury Watches',
+    'Custom Jewelry',
+  ],
+  'Baby and Kids': [
+    'Baby Gear (Strollers, Car Seats)',
+    'Kids\' Clothing',
+    'Toys and Games for Kids',
+  ],
+  'Pet Supplies': [
+    'Pet Food',
+    'Pet Accessories',
+    'Pet Grooming Products',
+  ],
+  'Office Supplies': [
+    'Stationery',
+    'Office Furniture',
+    'Electronics (Printers, Scanners)',
+  ],
+  'Art and Crafts': [
+    'Painting Supplies',
+    'Crafting Materials',
+    'DIY Kits',
+  ],
+  'Home Improvement': [
+    'Tools',
+    'Building Materials',
+    'Home Renovation Products',
+  ],
+  'Electrical Appliances': [
+    'Kitchen Appliances',
+    'Laundry Appliances',
+    'Home Climate Control',
+  ],
+  'Gifts and Occasions': [
+    'Gift Cards',
+    'Personalized Gifts',
+    'Party Supplies',
+  ],
+};
