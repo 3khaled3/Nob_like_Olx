5489fcb0f14c1cf2364fabd65d134510dcf281ce
3khaled3 <ff.tark123@gmail.com>
2023-08-05 09:31:38 +0300

    addLoginAutoAndfixCubit

--- lib/core/widget/tossetMassage.dart
+++ lib/core/widget/tossetMassage.dart
@@ -4,11 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 void showToastMessage(String message, Color backgroundColor) {
+  print("aaaaaaaaaaaaaaaaaaaaaaaaa${message.length*.4.round()}");
   Fluttertoast.showToast(
     msg: message,
     toastLength: Toast.LENGTH_SHORT,
     gravity: ToastGravity.BOTTOM,
-    timeInSecForIosWeb: 2,
+    timeInSecForIosWeb: 
+    5 ,
     backgroundColor: backgroundColor,
     textColor: Colors.white,
     fontSize: 16.0,
