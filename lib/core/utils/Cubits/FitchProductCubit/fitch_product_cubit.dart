af1b4e47c2161a425cad7ee0d93f7565c40a7b85
3khaled3 <ff.tark123@gmail.com>
2023-08-13 13:31:19 +0300

    fixFitchProductCubit

--- lib/core/utils/Cubits/FitchProductCubit/fitch_product_cubit.dart
+++ lib/core/utils/Cubits/FitchProductCubit/fitch_product_cubit.dart
@@ -2,7 +2,6 @@
 
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
 import 'package:nob/constant.dart';
 
@@ -10,33 +9,50 @@ part 'fitch_product_state.dart';
 
 class FitchProductCubit extends Cubit<FitchProductState> {
   FitchProductCubit() : super(FitchProductInitial());
+Future<Map<String, List>> getdata() async {
+  // Emit a waiting state here if needed
+  emit(Waitting());
 
-  Future<Map<String, List>> getdata() async {
-    emit(Waitting());
-    CollectionReference usersCollection =
-        FirebaseFirestore.instance.collection('ads');
-    final DocumentSnapshot Snaps =
-        await usersCollection.doc(FirebaseAuth.instance.currentUser!.uid).get();
-    List<dynamic> ads = [];
+  // Reference to the Firestore collection
+  CollectionReference adsCollection =
+      FirebaseFirestore.instance.collection('ads');
 
-    if (Snaps.exists) {
-      final Map<String, dynamic> data = Snaps.data() as Map<String, dynamic>;
-      ads = data["ads"] as List<dynamic>;
+  // Get all documents from the 'ads' collection
+  QuerySnapshot querySnapshot = await adsCollection.get();
+
+  List<dynamic> ads = [];
+
+  // Process each document in the query result
+  for (var documentSnapshot in querySnapshot.docs) {
+    if (documentSnapshot.exists) {
+      // Extract 'ads' array from the document data
+      final Map<String, dynamic> data = documentSnapshot.data()
+          as Map<String, dynamic>;
+      ads.addAll(data["ads"] as List<dynamic>);
     }
-    final List<String> Categories =
-        categories.map((categoryData) => categoryData.keys.first).toList();
+  }
 
-    Map<String, List> prodcts = {};
-    for (var i = 0; i < Categories.length; i++) {
-      prodcts[Categories[i]] = [];
-      for (var x = 0; x < ads.length; x++) {
-        if (ads[x]["product"]["categore"] == Categories[i]) {
-          prodcts[Categories[i]]?.add(ads[x]);
-        }
+  // Extract category names from 'categories' data
+  final List<String> Categories = categories
+      .map((categoryData) => categoryData.keys.first)
+      .toList();
+
+  // Map to hold categorized products
+  Map<String, List> products = {};
+
+  // Categorize products based on their 'categore' field
+  for (var i = 0; i < Categories.length; i++) {
+    products[Categories[i]] = [];
+    for (var x = 0; x < ads.length; x++) {
+      if (ads[x]["product"]["categore"] == Categories[i]) {
+        products[Categories[i]]?.add(ads[x]);
       }
     }
-    prodcts.removeWhere((category, products) => products.isEmpty);
-
-    return prodcts;
   }
+
+  // Remove categories with no products
+  products.removeWhere((category, products) => products.isEmpty);
+
+  return products;
+}
 }
