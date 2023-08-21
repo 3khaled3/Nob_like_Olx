dbf1fe890511fd5a5a7041eb4ddc11fc855ed956
3khaled3 <ff.tark123@gmail.com>
2023-08-21 06:45:33 +0300

    scroll

---
+++ lib/features/Chat/presentation/widget/message_builder.dart
@@ -0,0 +1,52 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../../data/message_data_model.dart';
+import 'masseges_bubles.dart';
+
+class MessageBuilder extends StatefulWidget {
+  final List<MessageDataModel> messages;
+  const MessageBuilder({
+    Key? key,
+    required this.messages,
+  }) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MessageBuilderState createState() => _MessageBuilderState();
+}
+
+class _MessageBuilderState extends State<MessageBuilder> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    // Scroll to the end initially (if messages are not empty).
+    if (widget.messages.isNotEmpty) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        controller: _scrollController, 
+        itemCount: widget.messages.length,
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (context, index) {
+          return widget.messages[index].sender ==
+                  FirebaseAuth.instance.currentUser!.uid
+              ? BuildSendMassegeBuble(
+                  containt: widget.messages[index].content,
+                  seen: widget.messages[index].isRead,
+                )
+              : BuildResiveMassegeBuble(containt: widget.messages[index].content);
+        },
+      ),
+    );
+  }
+}
