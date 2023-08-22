f16c3bb8f4abb0bbdcf50cc7a7f2e3ab4c35291b
3khaled3 <ff.tark123@gmail.com>
2023-08-22 04:43:05 +0300

    addMessageTime

--- lib/features/Chat/presentation/widget/masseges_bubles.dart
+++ lib/features/Chat/presentation/widget/masseges_bubles.dart
@@ -3,40 +3,74 @@ import 'package:flutter/material.dart';
 
 class BuildSendMassegeBuble extends StatelessWidget {
   final String containt;
+  final String time;
   final bool seen;
   const BuildSendMassegeBuble({
     super.key,
     required this.containt,
     required this.seen,
+    required this.time,
   });
 
   @override
   Widget build(BuildContext context) {
-    return BubbleSpecialThree(
-      sent: true,
-      seen: seen,
-      text: containt,
-      color: const Color(0xFF1B97F3),
-      tail: false,
-      textStyle: const TextStyle(color: Colors.white, fontSize: 16),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        const SizedBox(
+          height: 4,
+        ),
+        BubbleSpecialThree(
+          sent: true,
+          seen: seen,
+          text: containt,
+          color: const Color(0xFF1B97F3),
+          tail: false,
+          textStyle: const TextStyle(color: Colors.white, fontSize: 16),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 28),
+          child: Text(
+            time,
+            style: const TextStyle(color: Colors.grey, fontSize: 10),
+          ),
+        ),
+      ],
     );
   }
 }
 
 class BuildResiveMassegeBuble extends StatelessWidget {
   final String containt;
+  final String time;
   const BuildResiveMassegeBuble({
     super.key,
     required this.containt,
+    required this.time,
   });
 
   @override
   Widget build(BuildContext context) {
-    return BubbleSpecialThree(
-      text: containt,
-      color: const Color(0xFFE8E8EE),
-      tail: false,
-      isSender: false,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 4,
+        ),
+        BubbleSpecialThree(
+          text: containt,
+          color: const Color(0xFFE8E8EE),
+          tail: false,
+          isSender: false,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 28),
+          child: Text(
+            time,
+            style: const TextStyle(color: Colors.grey, fontSize: 10),
+          ),
+        ),
+      ],
     );
   }
 }
